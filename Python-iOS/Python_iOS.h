c5aad44f8e7698aecbb76b3874ee0245d00ab4d4
Yurii Samoienko <yurii.samoienko@gmail.com>
2022-05-06 17:27:37 +0300

    added Carthage support

---
+++ Python-iOS/Python_iOS.h
@@ -0,0 +1,18 @@
+//
+//  Python_iOS.h
+//  Python-iOS
+//
+//  Created by Yurii Samoienko on 06.05.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Python_iOS.
+FOUNDATION_EXPORT double Python_iOSVersionNumber;
+
+//! Project version string for Python_iOS.
+FOUNDATION_EXPORT const unsigned char Python_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Python_iOS/PublicHeader.h>
+
+
